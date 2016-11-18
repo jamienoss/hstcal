2fa15d3c9808e2b77c94bbd162eed782e66a0fb9
James Noss <jnoss@stsci.edu>
2016-11-18 16:58:24 -0500

    IT BUILDS!
    Signed-off-by: James Noss <jnoss@stsci.edu>

--- ctegen2/ctegen2.h
+++ ctegen2/ctegen2.h
@@ -1,9 +1,23 @@
 #ifndef CTEGEN2_INCL
 #define CTEGEN2_INCL
 
-//#include "../pkg/wfc3/calwf3/wf3cte/cte.h"
-#include "cte.h"
-#include "wf3.h"
+#include "hstio.h"
+
+//This may need to differ between instruments, but oh well for now.
+#define TRAPS 999 /*max number of traps per column = rows in pctetab[1], valid traps are < 999999 in qlev*/
+
+typedef struct {
+    unsigned n_forward; // number of forward modeling iterations
+    unsigned n_par; // number of iterations in parallel transfer
+    unsigned cte_traps; // number of valid TRAPS in file for reallocation
+    int cte_len; // max length of cte trail
+    double rn_amp; // read noise amplitude for clipping
+    double qlevq_data[TRAPS]; // charge packet size in electrons
+    double dpdew_data[TRAPS]; // trap size in electrons
+    FloatHdrData *rprof; // differential trail profile as image
+    FloatHdrData *cprof; // cumulative trail profile as image
+} CTEParams;
+
 int sim_colreadout_l(double * pixo, const double * pixf, const CTEParams * cte, const unsigned nRows);
 
 #endif
